f8a934c0927898fa9101ae8e595e791eefab0510
ZhangYuexin <zhangyuexin121@live.cn>
2019-11-11 10:53:57 +0800

    updat

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ service vsftpd restart
 # 安装 .NET Core Runtime
 wget https://download.visualstudio.microsoft.com/download/pr/0c5e013b-fa57-44dc-85bf-746885181278/58647e532fcc3a45209c13cdfbf30c74/dotnet-runtime-3.0.0-linux-arm.tar.gz
 # wget https://download.visualstudio.microsoft.com/download/pr/707fd000-c376-40de-9862-cabc46a344ec/82e0a3c816247bad4563c3e74655f7cf/dotnet-runtime-3.0.0-linux-arm64.tar.gz
-mkdir dotnet3 && tar -xvf dotnet-runtime-3.0.0-linux-arm.tar.gz -C ~/dotnet3
+mkdir dotnet3 && tar -xvf dotnet-runtime-3.0.0-linux-arm.tar.gz -C dotnet3
 ln -s dotnet3/dotnet /usr/bin/dotnet
 
 # 安装 Docker
